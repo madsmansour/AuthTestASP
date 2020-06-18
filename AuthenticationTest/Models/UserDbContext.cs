742689230c1b3e7a41895a2b25a43fc2aa533c5d
Mads Mansour Larsen <42993941+madsmansour@users.noreply.github.com>
2020-06-18 11:02:31 +0200

    init

---
+++ AuthenticationTest/Models/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationTest.Models
+{
+    public class UserDbContext : IdentityDbContext<Microsoft.AspNetCore.Identity.IdentityUser>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+                : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
